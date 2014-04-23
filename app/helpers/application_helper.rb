3d9aef63c4eb104b06a7222d1c8a4e544d3f2dac
kongr45gpen <electrovesta@gmail.com>
2014-04-23 16:23:47 +0300

    Update ruby version on .travis.yml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,17 +9,6 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
-
-  # change the default link renderer for will_paginate
-#   def will_paginate(collection_or_options = nil, options = {})
-#     if collection_or_options.is_a? Hash
-#       options, collection_or_options = collection_or_options, nil
-#     end
-#     unless options[:renderer]
-#       options = options.merge :renderer => SemanticUIPagination::SemanticUIRenderer
-#     end
-#     super *[collection_or_options, options].compact
-#   end
 end
 
 # A custom renderer class for WillPaginate that produces markup suitable for use with Semantic UI.
